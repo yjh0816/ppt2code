04194d06f6f5cd3f42a7ded0dfd51e3309246c95
yjh0816 <2543kt@naver.com>
2021-03-26 14:54:01 +0900

    new line add2

--- lab1.c
+++ lab1.c
@@ -5,8 +5,8 @@ int main() {
 	float f_temp;
 	printf("ȭ�� �µ��� �Է��ϼ��� : ");
 	scanf("%f",&f_temp);
-	
 	c_temp = (f_temp - 32.0) * 5.0 / 9.0;
+	
 	printf("���� �µ��� %.2f�� �Դϴ�", c_temp);
 
 	return 0;
